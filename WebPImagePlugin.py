4e8aa818847226c18eb31226b087d8ed1251c6c4
Shay Elkin <shay@everything.me>
2013-03-12 10:08:59 +0200

    Added decoder for RGBA images
    additional changes:
    * Consolidated the encode wrapper functions in webp.c
    * Added wrappers for WebPDecodeRGBA and WebPGetFeatures to webp.c
    * Removed unused variable quality_factor from WebPDecodeRGB_wrapper in webp.c
    * Changed _accept in WebPImagePlugin.py to match the documented WebP header (http://goo.gl/dBVdF)
    * Made _save in WebPImagePlugin.py raise IOError if mode isn't RGB or RGBA

--- WebPImagePlugin.py
+++ WebPImagePlugin.py
@@ -4,7 +4,7 @@ import StringIO
 import _webp
 
 def _accept(prefix):
-    return prefix[:4] == "RIFF" and prefix[8:16] == "WEBPVP8 "
+    return prefix[:4] == "RIFF" and prefix[8:12] == "WEBP"
 
 class WebPImageFile(ImageFile.ImageFile):
 
@@ -12,20 +12,30 @@ class WebPImageFile(ImageFile.ImageFile):
     format_description = "WebP image"
 
     def _open(self):
-        self.mode = "RGB"
-        data, width, height = _webp.WebPDecodeRGB(self.fp.read())
+        raw = self.fp.read()
+        fwidth, fheight, has_alpha = _webp.WebPGetFeatures(raw)                
+
+        if has_alpha:
+            self.mode = "RGBA"
+            data, width, height = _webp.WebPDecodeRGBA(raw)
+        else :
+            self.mode = "RGB"        
+            data, width, height = _webp.WebPDecodeRGB(raw)
+
+        assert (fwidth == width) and (fheight == height)
         self.size = width, height
         self.fp = StringIO.StringIO(data)
-        self.tile = [("raw", (0, 0) + self.size, 0, 'RGB')]
+        self.tile = [("raw", (0, 0) + self.size, 0, self.mode)]
 
 def _save(im, fp, filename):
 
-#        raise IOError("cannot write mode %s as WEBP" % im.mode)
     quality = im.encoderinfo.get("quality", 80)
     if im.mode == "RGB":    
     	data = _webp.WebPEncodeRGB(im.tostring(), im.size[0], im.size[1], im.size[0] * 3, float(quality))
-    else:
+    elif im.mode == "RGBA":
 	data = _webp.WebPEncodeRGBA(im.tostring(), im.size[0], im.size[1], im.size[0] * 4, float(quality))
+    else:
+        raise IOError("cannot write mode %s as WEBP" % im.mode)
     fp.write(data)
 
 Image.register_open("WEBP", WebPImageFile, _accept)
--- webp.c
+++ webp.c
@@ -2,22 +2,25 @@
 #include <webp/encode.h>
 #include <webp/decode.h>
 
-PyObject* WebPEncodeRGB_wrapper(PyObject* self, PyObject* args)
+typedef size_t (*WebPEncodeFunc)(const uint8_t*, int, int, int, float, uint8_t**);
+typedef uint8_t* (*WebPDecodeFunc)(const uint8_t*, size_t, int*, int *);
+
+PyObject* encode_wrapper(WebPEncodeFunc encode_func, PyObject* self, PyObject* args)
 {
-    PyStringObject *rgb_string;
+    PyStringObject *data_string;
     int width;
     int height;
     int stride;
     float quality_factor;
 
-    if (!PyArg_ParseTuple(args, "Siiif", &rgb_string, &width, &height, &stride, &quality_factor)) {
+    if (!PyArg_ParseTuple(args, "Siiif", &data_string, &width, &height, &stride, &quality_factor)) {
         Py_INCREF(Py_None);
         return Py_None;
     }
     
-    uint8_t *rgb;
+    uint8_t *data;
     Py_ssize_t size;
-    PyString_AsStringAndSize((struct PyObject *) rgb_string, &rgb, &size);
+    PyString_AsStringAndSize((struct PyObject *) data_string, &data, &size);
 
     if (stride * height > size) {
         Py_INCREF(Py_None);
@@ -25,7 +28,7 @@ PyObject* WebPEncodeRGB_wrapper(PyObject* self, PyObject* args)
     }
 
     uint8_t *output;
-    size_t ret_size = WebPEncodeRGB(rgb, width, height, stride, quality_factor, &output);
+    size_t ret_size = encode_func(data, width, height, stride, quality_factor, &output);
     if (ret_size > 0) {
         PyObject *ret = PyString_FromStringAndSize(output, ret_size);
         free(output);
@@ -33,47 +36,21 @@ PyObject* WebPEncodeRGB_wrapper(PyObject* self, PyObject* args)
     }
     Py_INCREF(Py_None);
     return Py_None;
+}
 
+PyObject* WebPEncodeRGB_wrapper(PyObject* self, PyObject* args) 
+{
+    return encode_wrapper(&WebPEncodeRGB, self, args);
 }
 
 PyObject* WebPEncodeRGBA_wrapper(PyObject* self, PyObject* args)
 {
-    PyStringObject *rgba_string;
-    int width;
-    int height;
-    int stride;
-    float quality_factor;
-
-    if (!PyArg_ParseTuple(args, "Siiif", &rgba_string, &width, &height, &stride, &quality_factor)) {
-        Py_INCREF(Py_None);
-        return Py_None;
-    }
-    
-    uint8_t *rgba;
-    Py_ssize_t size;
-    PyString_AsStringAndSize((struct PyObject *) rgba_string, &rgba, &size);
-
-    if (stride * height > size) {
-        Py_INCREF(Py_None);
-        return Py_None;
-    }
-
-    uint8_t *output;
-    size_t ret_size = WebPEncodeRGBA(rgba, width, height, stride, quality_factor, &output);
-    if (ret_size > 0) {
-        PyObject *ret = PyString_FromStringAndSize(output, ret_size);
-        free(output);
-        return ret;
-    }
-    Py_INCREF(Py_None);
-    return Py_None;
-
+    return encode_wrapper(&WebPEncodeRGBA, self, args);
 }
 
-PyObject* WebPDecodeRGB_wrapper(PyObject* self, PyObject* args)
+PyObject* decode_wrapper(WebPDecodeFunc decode_func, PyObject* self, PyObject* args)
 {
     PyStringObject *webp_string;
-    float quality_factor;
 
     if (!PyArg_ParseTuple(args, "S", &webp_string)) {
         Py_INCREF(Py_None);
@@ -81,7 +58,7 @@ PyObject* WebPDecodeRGB_wrapper(PyObject* self, PyObject* args)
     }    
     uint8_t *webp;
     Py_ssize_t size;
-    PyString_AsStringAndSize((struct PyObject *) webp_string, &webp, &size);
+    PyString_AsStringAndSize((struct PyObject *) webp_string, &webp, &size);  
 
     int width;
     int height;
@@ -92,11 +69,50 @@ PyObject* WebPDecodeRGB_wrapper(PyObject* self, PyObject* args)
     return Py_BuildValue("Sii", ret, width, height);
 }
 
+PyObject* WebPDecodeRGB_wrapper(PyObject* self, PyObject* args) 
+{
+    return decode_wrapper(&WebPDecodeRGB, self, args);
+}
+
+PyObject* WebPDecodeRGBA_wrapper(PyObject* self, PyObject* args) 
+{
+    return decode_wrapper(&WebPDecodeRGBA, self, args);
+}
+
+PyObject* WebPGetFeatures_wrapper(PyObject* self, PyObject* args)
+{
+    PyStringObject *webp_string;
+
+    if (!PyArg_ParseTuple(args, "S", &webp_string)) {
+        Py_INCREF(Py_None);
+        return Py_None;
+    }    
+    uint8_t *webp;
+    Py_ssize_t size;
+    PyString_AsStringAndSize((struct PyObject *) webp_string, &webp, &size);  
+
+    WebPBitstreamFeatures features;   
+    VP8StatusCode err = WebPGetFeatures(webp, size, &features);
+
+    if (VP8_STATUS_OK != err) {
+        return PyErr_Format(PyExc_RuntimeError, "WebPGetFeatures returned error: %d", err);
+    }
+
+    return Py_BuildValue("iii", features.width, features.height, features.has_alpha);
+}
+
 static PyMethodDef webpMethods[] =
 {
-    {"WebPEncodeRGB", WebPEncodeRGB_wrapper, METH_VARARGS, "WebPEncodeRGB"},
-    {"WebPEncodeRGBA", WebPEncodeRGBA_wrapper, METH_VARARGS, "WebPEncodeRGBA"},
-    {"WebPDecodeRGB", WebPDecodeRGB_wrapper, METH_VARARGS, "WebPEncodeRGB"},
+    {"WebPEncodeRGB", WebPEncodeRGB_wrapper, METH_VARARGS, 
+        "Wraps WebPEncodeRGB\nArguments: data, width, height, stride, quality_factor\nReturns encoded image as Python String"},
+    {"WebPEncodeRGBA", WebPEncodeRGBA_wrapper, METH_VARARGS,
+        "Wraps WebPEncodeRGBA\nArguments: data, width, height, stride, quality_factor\nReturns encoded image as Python String"},
+    {"WebPDecodeRGB", WebPDecodeRGB_wrapper, METH_VARARGS, 
+        "Wraps WebPDecodeRGB\nReturns a (raw_data, width, height) tuple, where raw_data is the image samples in RGB order"},
+    {"WebPDecodeRGBA", WebPDecodeRGBA_wrapper, METH_VARARGS,
+        "Wraps WebPDecodeRGBA\nReturns a (raw_data, width, height) tuple, where raw_data is the image samples in RGBA order"},
+    {"WebPGetFeatures", WebPGetFeatures_wrapper, METH_VARARGS,
+        "Wraps WebPGetFeatures\nReturns a (width, height, has_alpha) tuple"},
     {NULL, NULL}
 };
 
