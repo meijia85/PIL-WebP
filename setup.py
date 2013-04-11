1eaafec96e1974d5bfaf9e5e2cd09efd63221894
Dvir Volk <dvir@everything.me>
2013-04-12 00:37:34 +0300

    fixed things so that setup.py will actually install something...

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup, Extension
 
 setup(name = 'WebPImagePlugin',
       version = '0.1',
+      package_dir={'WebPImagePlugin': '.'},
       description = 'File decoder an encoder for Python Imaging Library',
       url = 'https://github.com/EverythingMe/PIL-WebP',
+	packages = ['WebPImagePlugin'],
       ext_modules = [Extension('_webp_wrapper', 
                                sources = ['_webp_wrapper.c'],
                                libraries = ['webp'])]
