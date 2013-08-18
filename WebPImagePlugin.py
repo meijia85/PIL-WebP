ec3dcd13cf8f0c08aed2ceed6210131e8975600e
Roey Berman <roey@everything.me>
2013-08-18 12:02:33 +0300

    try to read data using file io instead of StringIO

--- WebPImagePlugin.py
+++ WebPImagePlugin.py
@@ -24,7 +24,10 @@ class WebPImageFile(ImageFile.ImageFile):
 
         assert (fwidth == width) and (fheight == height)
         self.size = width, height
-        self.fp = StringIO.StringIO(data)
+        #self.fp = StringIO.StringIO(data)
+        with open('/tmp/123.webp', 'wb') as f:
+            f.write(data)
+        self.fp = open('/tmp/123.webp', 'rb')
         self.tile = [("raw", (0, 0) + self.size, 0, self.mode)]
 
 def _save(im, fp, filename):
